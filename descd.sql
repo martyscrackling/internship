5556991826feecb5cf132d1dfdb3373bd75a7595
Your Name <your-email@example.com>
2025-04-02 06:31:24 +0800

    updated

--- descd.sql
+++ descd.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 24, 2025 at 02:56 PM
+-- Generation Time: Apr 02, 2025 at 12:29 AM
 -- Server version: 10.11.11-MariaDB
 -- PHP Version: 8.2.12
 
@@ -20,6 +20,33 @@ SET time_zone = "+00:00";
 --
 -- Database: `descd`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `announcements`
+--
+
+CREATE TABLE `announcements` (
+  `announcement_id` int(11) NOT NULL,
+  `a_title` varchar(255) NOT NULL,
+  `a_description` longtext NOT NULL,
+  `a_date` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `announcements`
+--
+
+INSERT INTO `announcements` (`announcement_id`, `a_title`, `a_description`, `a_date`) VALUES
+(1, 'Testing only if the announcement is working!', 'On March 20, 2025, Western Mindanao State University (WMSU) and Police Regional Office 9 (PRO-9) signed a memorandum of understanding (MOU) for the internship program of WMSU&#039;s Bachelor of Science in Criminology students.\r\n\r\nPBGEN Rodolfo emphasized ', '2025-04-01'),
+(2, 'ADD A SECOND ANNOUNCEMENT', 'hahahaahahah eto ay pangalawa!!!!!!1', '2025-04-01'),
+(3, 'lorem 255', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vel turpis at odio vestibulum convallis. Sed facilisis justo id dui ullamcorper, nec placerat nisl sagittis. Phasellus sit amet orci vel odio faucibus lacinia. Fusce id velit eu mauris ferme', '2025-04-01'),
+(8, 'lorem hahahaa', 'wala lan hahahaha', '2025-04-01'),
+(9, 'lorem 1k', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\r\n\r\nVestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Curabitur aliquet quam id dui posuere blandit. Nulla porttitor accumsan tincidunt. Donec sollicitudin molestie malesuada. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Donec sollicitudin molestie malesuada. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui.\r\n\r\nCras ultricies ligula sed magna dictum porta. Pellentesque in ipsum id orci porta dapibus. Curabitur aliquet quam id dui posuere blandit. Quisque velit nisi, pretium ut lacinia in, elementum id enim. Nulla quis lorem ut libero malesuada feugiat. Nulla quis lorem ut libero malesuada feugiat. Vivamus suscipit tortor eget felis porttitor volutpat. Donec rutrum congue leo eget malesuada. Proin eget tortor risus.\r\n\r\nPhasellus eget enim eu lectus faucibus vestibulum. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Donec sollicitudin molestie malesuada. Cras ultricies ligula sed magna dictum porta. Curabitur aliquet quam id dui posuere blandit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque velit nisi, pretium ut lacinia in, elementum id enim. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a.\r\n\r\nAliquam sit amet gravida nibh. Donec rutrum congue leo eget malesuada. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Sed porttitor lectus nibh. Proin eget tortor risus. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Proin eget tortor risus. Sed porttitor lectus nibh. Quisque velit nisi, pretium ut lacinia in, elementum id enim. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem.\r\n\r\nVestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Cras ultricies ligula sed magna dictum porta. Curabitur aliquet quam id dui posuere blandit. Nulla quis lorem ut libero malesuada feugiat. Proin eget tortor risus. Donec sollicitudin molestie malesuada. Proin eget tortor risus. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui.', '2025-04-01'),
+(10, 'add', 'number4', '0000-00-00'),
+(11, 'test', 'date', '2025-04-01'),
+(12, 'last test', 'hahahaha', 'April 1, 2025');
 
 -- --------------------------------------------------------
 
@@ -50,6 +77,12 @@ INSERT INTO `login` (`user_id`, `firstname`, `lastname`, `username`, `email`, `p
 --
 
 --
+-- Indexes for table `announcements`
+--
+ALTER TABLE `announcements`
+  ADD PRIMARY KEY (`announcement_id`);
+
+--
 -- Indexes for table `login`
 --
 ALTER TABLE `login`
@@ -58,6 +91,12 @@ ALTER TABLE `login`
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `announcements`
+--
+ALTER TABLE `announcements`
+  MODIFY `announcement_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 
 --
 -- AUTO_INCREMENT for table `login`
